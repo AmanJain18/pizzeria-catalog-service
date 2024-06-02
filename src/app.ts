c50b078d0eb38eadc512fed4fb88cfd997db8b44
Aman Jain <amansjain.aj@gmail.com>
2024-06-02 23:18:52 +0530

    setup initial express app config - cors, etc

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { globalErrorHandler } from './middlewares/globalErrorHandler';
+import { configSetup } from './config/configSetup';
 
 const app = express();
+
+configSetup(app);
 
 app.get('/', (req, res) => {
     res.send('Welcome to catalog service');
---
+++ src/config/configSetup.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import config from 'config';
+
+export const configSetup = (app: express.Application) => {
+    app.use(
+        cors({
+            origin:
+                config.get('catalogService.cors.corsOrigin') ||
+                'http://localhost:3000',
+            credentials: true,
+        }),
+    );
+
+    app.use(express.static('public'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+};
