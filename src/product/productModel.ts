9e7cde044da30890e5050f17a4a04ccf70165ab9
Aman Jain <amansjain.aj@gmail.com>
2024-06-03 11:39:44 +0530

    add product model

---
+++ src/product/productModel.ts
@@ -0,0 +1,70 @@
+// Purpose: Model for categories.
+
+import mongoose from 'mongoose';
+
+const priceConfigurationSchema = new mongoose.Schema({
+    priceType: {
+        type: String,
+        enum: ['base', 'additional'],
+        required: true,
+    },
+    sizeOptions: {
+        type: Map,
+        of: Number,
+        required: true,
+    },
+});
+
+const attributeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+});
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        priceConfiguration: {
+            type: Map,
+            of: priceConfigurationSchema,
+            required: true,
+        },
+        attributes: {
+            type: [attributeSchema],
+            required: true,
+        },
+        tenantId: {
+            type: String,
+            required: true,
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Category',
+            required: true,
+        },
+        isPublished: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model('Product', productSchema);
