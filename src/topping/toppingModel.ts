6fa72fbe8ff8b942f13eb493c433cc115a0cf1ad
Aman Jain <amansjain.aj@gmail.com>
2024-06-25 21:02:52 +0530

    add topping model

---
+++ src/topping/toppingModel.ts
@@ -0,0 +1,33 @@
+// Purpose: Model for toppings.
+
+import mongoose from 'mongoose';
+import { Topping } from './toppingTypes';
+
+const toppingSchema = new mongoose.Schema<Topping>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        tenantId: {
+            type: String,
+            required: true,
+        },
+        isPublish: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model('Topping', toppingSchema);
