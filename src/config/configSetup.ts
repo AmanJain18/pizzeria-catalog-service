36320640ebe6a98e8e93017eea1a203d5de58e7a
Aman Jain <amansjain.aj@gmail.com>
2024-06-26 04:22:03 +0530

    add cors policy and message

--- src/config/configSetup.ts
+++ src/config/configSetup.ts
@@ -3,10 +3,21 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import config from 'config';
 
+const allowedOrigins: string[] = config.get('cors.corsOrigin');
+
 export const configSetup = (app: express.Application) => {
     app.use(
         cors({
-            origin: config.get('cors.corsOrigin') || 'http://localhost:3000',
+            origin: (origin, callback) => {
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    const msg =
+                        'The CORS policy for this site does not ' +
+                        'allow access from the specified Origin.';
+                    return callback(new Error(msg), false);
+                }
+                return callback(null, true);
+            },
             credentials: true,
         }),
     );
