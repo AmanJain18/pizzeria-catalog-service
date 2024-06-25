6ec3fbb6ac8aad61233451952c7439546bf4dc19
Aman Jain <amansjain.aj@gmail.com>
2024-06-25 20:50:38 +0530

    update product types

--- src/product/productTypes.ts
+++ src/product/productTypes.ts
@@ -1,31 +1,72 @@
+import { Request } from 'express';
+import { UploadedFile } from 'express-fileupload';
 import mongoose from 'mongoose';
 
 export interface PriceConfiguration {
     [key: string]: {
         priceType: 'base' | 'additional';
-        sizeOptions: number[];
+        sizeOptions: {
+            [key: string]: number;
+        };
     };
 }
 
 export interface Attribute {
     name: string;
-    value: string;
+    value: string | boolean | number;
 }
 
-export interface Product {
-    _id?: mongoose.Types.ObjectId;
+export interface BaseProduct {
     name: string;
     description: string;
     image: string;
-    priceConfiguration: string;
-    attributes: string;
+    priceConfiguration: PriceConfiguration;
+    attributes: Attribute[];
     tenantId: string;
     categoryId: string;
     isPublished: boolean;
 }
 
+export interface CreateProduct extends BaseProduct {}
+
+export interface UpdateProduct extends BaseProduct {}
+
 export interface QueryFilters {
     tenantId?: string;
     categoryId?: mongoose.Types.ObjectId;
     isPublished?: boolean;
+}
+
+export interface Product extends BaseProduct {
+    _id?: mongoose.Types.ObjectId;
+}
+
+export interface CreateProductRequest extends Request {
+    body: {
+        name: string;
+        description: string;
+        priceConfiguration: string;
+        attributes: string;
+        tenantId: string;
+        categoryId: string;
+        isPublished?: boolean;
+    };
+    files: {
+        image: UploadedFile;
+    };
+}
+
+export interface UpdateProductRequest extends Request {
+    body: {
+        name: string;
+        description: string;
+        priceConfiguration: string;
+        attributes: string;
+        tenantId: string;
+        categoryId: string;
+        isPublished?: boolean;
+    };
+    files?: {
+        image?: UploadedFile;
+    };
 }
